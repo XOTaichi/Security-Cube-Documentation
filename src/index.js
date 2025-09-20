245c793c89aef99dbfbd7499183c94c40fe10143
XOTaiChi <linuxwolfie@sjtu.edu.cn>
2025-09-20 11:48:28 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; 
+import { HashRouter as Router } from "react-router-dom";  // 引入 HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* 使用 HashRouter 包裹 App */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
